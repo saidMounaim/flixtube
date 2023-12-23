60cde05ba87958fcc171639252910f7d6c02f81a
Said Mounaim <saidmounaim00@gmail.com>
2023-12-23 12:31:24 +0100

    Add Sign Up Page

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,5 +1,36 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+
 const SignUpPage = () => {
-  return <div>SignUpPage</div>;
+  return (
+    <div className="mt-24 rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14 ">
+      <form>
+        <h1 className="text-3xl font-semibold text-white ">Sign Up</h1>
+        <div className="space-y-4 mt-5 ">
+          <Input
+            type="email"
+            name="email"
+            placeholder="Email"
+            className="bg-[#333] placeholder:text-xs placeholder:text-gray-500 w-full inline-block"
+          />
+          <Button
+            type="submit"
+            variant="destructive"
+            className="w-full bg-[#e50914]"
+          >
+            Sign Up
+          </Button>
+        </div>
+      </form>
+      <div className="text-gray-500 text-sm mt-3">
+        Already Have an account?{" "}
+        <Link href="/login" className="text-white hover:underline">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
